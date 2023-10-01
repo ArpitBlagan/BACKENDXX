9a31ae87961024f346bddad30bdc02fbfad96cfd
arpit <blaganarpit@gmail.com>
2023-10-01 12:02:08 +0530

    done

--- controller/user.js
+++ controller/user.js
@@ -44,7 +44,7 @@ exports.info=async(req,res)=>{
     res.json({message:"working"});
 }
 exports.check=async(req,res)=>{
-    console.log(req.user);
+    console.log("not working",req.user);
     if(req.user){return res.json({message:"authorized",user:req.user})}
     const token=req.cookies.jwt;console.log(token);
     if(token){
