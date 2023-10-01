89b5fcbd4397d3ba9055b79e9b49b724a3a68c03
ArpitBlagan <114584343+ArpitBlagan@users.noreply.github.com>
2023-10-01 20:46:57 +0530

    Update Router.js

--- Router.js
+++ Router.js
@@ -2,8 +2,8 @@ const express=require('express');
 const { register,login,info,check } =require('./controller/user');
 const passport = require('passport');
 const Router=express.Router();
-const successLoginUrl = "https://65194e37b441053f3e1b3418--warm-starburst-506116.netlify.app/main";
-const errorLoginUrl = "https://65194e37b441053f3e1b3418--warm-starburst-506116.netlify.app";
+const successLoginUrl = "https://65198cff99e6aa618a413198--peaceful-taffy-b3c774.netlify.app/main";
+const errorLoginUrl = "https://65198cff99e6aa618a413198--peaceful-taffy-b3c774.netlify.app";
 Router.route('/register').post(register);
 Router.route('/login').post(login);
 Router.get(
@@ -26,14 +26,14 @@ Router.get(
       failureMessage: "Cannot login to Google, please try again later!",failureRedirect: errorLoginUrl,
     }),
     (req, res) => {
-        console.log(req.user);
+        console.log("user",req.user);
         res.cookie("name",req.user.name ,{
             //30 days in milisecond
             httpOnly:true,
             sameSite: 'none',
             secure:true
         });
-        res.redirect("https://651940918339e837a02e3661--courageous-boba-83d994.netlify.app/main");
+        res.redirect("https://65198cff99e6aa618a413198--peaceful-taffy-b3c774.netlify.app/main");
     }
 );
 // Router.get('/info',passport.authenticate('jwt', { session: false }),info);
